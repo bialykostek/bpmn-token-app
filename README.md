db6a1ad95c8b6375beed161f3cca18cae14978e5
Jasiek Kostecki <bialykostek@gmail.com>
2023-05-07 15:12:44 +0200

    core app

---
+++ README.md
@@ -0,0 +1,15 @@
+### Aplikacja do wizualizacji tokenów BPMN
+
+# Instalation
+npm install bpmn-js-token-simulation
+npm install
+npm run start:app
+
+# Build
+npm bundle
+
+Created by:
+Adrianna Pączek
+Jan Kostecki
+
+2023
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-env node */
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+
+
+module.exports = (env, argv) => {
+
+  const mode = argv.mode || 'development';
+
+  const devtool = mode === 'development' ? 'eval-source-map' : 'source-map';
+
+  return {
+    mode,
+    entry: {
+      viewer: './app/src/viewer.js',
+      modeler: './app/src/modeler.js'
+    },
+    output: {
+      filename: 'dist/[name].js',
+      path: __dirname + '/app'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.bpmn$/,
+          type: 'asset/source'
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './assets', to: 'dist/vendor/bpmn-js-token-simulation/assets' },
+          { from: 'bpmn-js/dist/assets', context: 'node_modules', to: 'dist/vendor/bpmn-js/assets' },
+          { from: 'bpmn-js-properties-panel/dist/assets', context: 'node_modules', to: 'dist/vendor/bpmn-js-properties-panel/assets' }
+        ]
+      }),
+      new DefinePlugin({
+        'process.env.TOKEN_SIMULATION_VERSION': JSON.stringify(require('./package.json').version)
+      })
+    ],
+    devtool
+  };
+
+};
