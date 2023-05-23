ff357782ae3078ab09dd8649af21e2763a1c00c6
Jasiek Kostecki <bialykostek@gmail.com>
2023-05-23 18:36:45 +0200

    token couter test

--- app/src/viewer.js
+++ app/src/viewer.js
@@ -123,10 +123,23 @@ function openDiagram(diagram) {
       viewer.get('canvas').zoom('fit-viewport');
       document.querySelector('.bts-palette').innerHTML += "<b>Auto simulation</b><br />"
       document.querySelector('.bts-palette').innerHTML += 'Tokens: <input type="number" id="tokens" style="width: 3em" value=10 /><br />'
-      document.querySelector('.bts-palette').innerHTML += 'Lamda: <input type="number" id="lambda" style="width: 3em" value=5 /><br />'
+      document.querySelector('.bts-palette').innerHTML += 'Lambda: <input type="number" id="lambda" style="width: 3em" value=5 /><br />'
       document.querySelector('.bts-palette').innerHTML += '<div class="bts-entry" id="autoTriggerBtn" title="Play/Pause Auto"><span class="bts-icon "><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg></span></div>';
       document.getElementById('autoTriggerBtn').addEventListener('click', autoTrigger);
       
+      var visited = {};
+      document.querySelectorAll('.djs-overlays').forEach((val, ind) => {
+          val.id = "block" + ind;
+          visited[val.id] = [];
+      })
+
+      setInterval(_ => {
+        console.log(visited)
+      }, 1000)
+      
+console.log(viewer)
+
+
     })
     .catch(err => {
       console.error(err);
